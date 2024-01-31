a80725bb39fb52b5fde7e3e486adb02b1994c1c5
Valery Klachkov <zgdump@gmail.com>
2024-02-01 00:21:22 +0300

    Первые эксперименты с SANE

--- simple-sane/src/lib.rs
+++ simple-sane/src/lib.rs
@@ -1,3 +1,4 @@
 #![allow(unused)]
 
 mod ffi;
+mod scanner;
---
+++ simple-sane/src/scanner.rs
@@ -0,0 +1,93 @@
+use std::{
+    ffi::CStr,
+    fmt::Display,
+    ptr::{null, null_mut},
+};
+
+use crate::ffi;
+
+macro_rules! sane_try {
+    ($x:expr) => {{
+        let status = $x;
+        if status != ffi::SANE_Status_SANE_STATUS_GOOD {
+            return status;
+        }
+    }};
+}
+
+pub struct Scanner {}
+
+impl Scanner {
+    pub unsafe fn experiments() -> ffi::SANE_Status {
+        // Setup SANE
+        sane_try!(ffi::sane_init(null_mut(), None));
+
+        // Get device
+        let mut device_list = null_mut();
+        sane_try!(ffi::sane_get_devices(&mut device_list, 0));
+
+        if device_list.is_null() {
+            println!("No devices!");
+            return ffi::SANE_Status_SANE_STATUS_GOOD;
+        }
+
+        Self::print_devices(device_list);
+
+        // Open first device. Just for tests
+        let device = *device_list;
+        let mut device_handle = null_mut();
+
+        sane_try!(ffi::sane_open((*device).name, &mut device_handle));
+
+        // TODO: How to setup scanner?
+
+        // Start scan
+        sane_try!(ffi::sane_start(device_handle));
+
+        let mut image = Vec::with_capacity(10 * 1024);
+        loop {
+            let mut buffer = [0; 1024];
+            let mut read = 0;
+
+            // Read until EOF
+            sane_try!(ffi::sane_read(
+                device_handle,
+                buffer.as_mut_ptr(),
+                buffer.len().try_into().unwrap_or(i32::MAX),
+                &mut read
+            ));
+
+            if read == 0 {
+                break;
+            }
+
+            image.extend(buffer);
+        }
+
+        std::fs::write("test.bin", image).unwrap();
+
+        // Cleanup, stop SANE
+        ffi::sane_exit();
+
+        ffi::SANE_Status_SANE_STATUS_GOOD
+    }
+
+    unsafe fn print_devices(device_list: *mut *const ffi::SANE_Device) {
+        let mut device = device_list;
+        loop {
+            if device.is_null() {
+                break;
+            }
+
+            println!(
+                "Scanner name '{name}', model '{model}', vendor '{vendor}', type '{ty}'",
+                name = CStr::from_ptr((**device).name).to_string_lossy(),
+                model = CStr::from_ptr((**device).model).to_string_lossy(),
+                vendor = CStr::from_ptr((**device).vendor).to_string_lossy(),
+                ty = CStr::from_ptr((**device).type_).to_string_lossy(),
+            );
+
+            device = device.add(1);
+        }
+    }
+}
