4452984182dfa6af404033b8ba22575a646b2a58
Valery Klachkov <zgdump@gmail.com>
2024-03-03 01:38:30 +0300

    bot: Поддержка печати DOCX и TXT файлов

--- mfp3000bot/src/print.rs
+++ mfp3000bot/src/print.rs
@@ -1,9 +1,16 @@
-use anyhow::{anyhow, bail};
+use anyhow::{anyhow, bail, Context};
 use cstr::cstr;
-use reqwest::{blocking::get, Url};
+use reqwest::{
+    blocking::{get, Client},
+    Url,
+};
 use simple_cups::{
     options::{ColorMode, MediaFormat, Options, Orientation, PrintQuality, Sides},
     printer::{Document, DocumentType, Printer},
+};
+use std::{
+    io::{self, Read},
+    time::Instant,
 };
 
 pub fn print_remote_file(printer: &str, docname: &String, url: &Url) -> anyhow::Result<()> {
@@ -12,18 +19,29 @@ pub fn print_remote_file(printer: &str, docname: &String, url: &Url) -> anyhow::
             bail!("printer '{printer}' not found");
         };
 
-        let mut document_reader = get(url.to_owned()).map_err(|err| {
+        let document_reader = get(url.to_owned()).map_err(|err| {
             anyhow!(
                 "failed to download document '{docname}': {}",
                 err.without_url()
             )
         })?;
 
+        let (ty, mut reader): (_, Box<dyn io::Read>) = if docname.ends_with(".txt") {
+            (DocumentType::PlainText, Box::new(document_reader))
+        } else if docname.ends_with(".pdf") {
+            (DocumentType::PDF, Box::new(document_reader))
+        } else if docname.ends_with(".docx") {
+            let pdf = docx_to_pdf(document_reader)?;
+            (DocumentType::PDF, Box::new(io::Cursor::new(pdf)))
+        } else {
+            bail!("Unsupported file format");
+        };
+
         // TODO: Support plaintext, images and docx.
         let document = Document {
             file_name: docname,
-            ty: DocumentType::PDF,
-            reader: &mut document_reader,
+            ty,
+            reader: &mut reader,
         };
 
         // TODO: Move options to the config.
@@ -39,4 +57,71 @@ pub fn print_remote_file(printer: &str, docname: &String, url: &Url) -> anyhow::
 
         Ok(())
     })
+}
+
+fn docx_to_pdf(mut reader: impl io::Read) -> anyhow::Result<Vec<u8>> {
+    use base64::{engine::general_purpose, read::DecoderReader, write::EncoderStringWriter};
+    use iter_read::IterRead;
+
+    let mut docx_base64_encoder = EncoderStringWriter::new(&general_purpose::STANDARD);
+    io::copy(&mut reader, &mut docx_base64_encoder)
+        .context("copying data from pdf to base64 encoder")?;
+
+    let request = format!(
+        r#"<?xml version="1.0"?>
+           <methodCall>
+             <!-- Method description here: https://github.com/unoconv/unoserver/blob/ecaea93bced40ab7e544eb4e2a89bd2d13b4788d/src/unoserver/converter.py#L142 -->
+             <methodName>convert</methodName>
+             <params>
+               <!-- inpath  --> <param><value><nil/></value></param>
+               <!-- indata  --> <param><value><base64>{document}</base64></value></param>
+               <!-- outpath --> <param><value><nil/></value></param>
+               <!-- format  --> <param><value><string>pdf</string></value></param>
+             </params>
+           </methodCall>
+    "#,
+        document = docx_base64_encoder.into_inner()
+    );
+
+    // TODO: Move unoserver url to the config
+    // TODO: Use bytes() instead of text()
+    let pdf_response = Client::new()
+        .post("http://localhost:2003/")
+        .body(request)
+        .send()
+        .context("sending request to unoserver")?
+        .text()
+        .context("reading response from unoserver")?;
+
+    let base64_open_tag = "<base64>";
+    let base64_close_tag = "</base64>";
+
+    let pdf_base64_start_idx = pdf_response
+        .find(base64_open_tag)
+        .map(|p| p + base64_open_tag.len())
+        .ok_or_else(|| {
+            anyhow!("failed to find <base64> in unoserver response: '{pdf_response}'")
+        })?;
+
+    let pdf_base64_end_idx = pdf_response.rfind(base64_close_tag).ok_or_else(|| {
+        anyhow!("failed to find </base64> in unoserver response: '{pdf_response}'")
+    })?;
+
+    let instant = Instant::now();
+
+    // It seems to me that this code should not panic,
+    // because the unoserver response will always only contain ascii characters?..
+    let pdf_base64 = &pdf_response.as_bytes()[pdf_base64_start_idx..pdf_base64_end_idx];
+
+    // TODO: Is it fast? Check and, maybe, write custom iter read with filter based on memchr.
+    let pdf_base64_reader = IterRead::new(pdf_base64.into_iter().copied().filter(|&b| b != 10));
+
+    let mut pdf = Vec::new();
+    DecoderReader::new(pdf_base64_reader, &general_purpose::STANDARD)
+        .read_to_end(&mut pdf)
+        .context("decoding base64 encoded pdf from unoserver")?;
+
+    log::debug!("Time spent decoding base64: {:?}", instant.elapsed());
+
+    Ok(pdf)
 }
