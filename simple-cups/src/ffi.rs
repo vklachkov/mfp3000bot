942748281233eb9864c25d3b831a24407d2e09f1
Valery Klachkov <zgdump@gmail.com>
2024-03-03 11:28:57 +0300

    simple_cups: ffi: Поправлены настройки линтера под bindgen

--- simple-cups/src/ffi.rs
+++ simple-cups/src/ffi.rs
@@ -1,3 +1,9 @@
-#![allow(non_camel_case_types, non_upper_case_globals, unused, improper_ctypes)]
+#![allow(
+    non_snake_case,
+    non_camel_case_types,
+    non_upper_case_globals,
+    unused,
+    improper_ctypes
+)]
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
