f1c7ccca25ff84f8707395d02c22770c1b38c349
Valery Klachkov <zgdump@gmail.com>
2024-03-11 15:04:27 +0300

    simple_sane: Отключен варнинг для тестов bindgen

--- simple-sane/src/ffi.rs
+++ simple-sane/src/ffi.rs
@@ -1,3 +1,9 @@
-#![allow(non_camel_case_types, non_upper_case_globals, unused, improper_ctypes)]
+#![allow(
+    non_camel_case_types,
+    non_upper_case_globals,
+    non_snake_case,
+    unused,
+    improper_ctypes
+)]
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
