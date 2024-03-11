e7c04b885ce559a5574da6a354ba74cf8b52037e
Valery Klachkov <zgdump@gmail.com>
2024-03-11 11:01:00 +0300

    Исправлена работа скрипта для сборки под Raspberry Pi Zero после перемещения в папку build

--- build/RPiZero.sh
+++ build/RPiZero.sh
@@ -1,10 +1,13 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 set -ue
 
-docker build -f RPiZero.Dockerfile -t mfp3000bot_build .
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+IMAGE_NAME=mfp3000bot_build
+
+docker build -f "$SCRIPT_DIR/RPiZero.Dockerfile" -t "$IMAGE_NAME" .
 
 docker run \
-    -v $HOME/.cargo/registry:/root/.cargo/registry \
-    -v $(pwd):/src \
-    -t mfp3000bot_build
+    -v "$HOME/.cargo/registry:/root/.cargo/registry" \
+    -v "$SCRIPT_DIR/..:/src" \
+    -t "$IMAGE_NAME"
