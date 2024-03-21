62621b145fc765919c64ce97468dc6807d3e284c
Valery Klachkov <zgdump@gmail.com>
2024-03-21 13:35:30 +0300

    libcups: Исправлена сборка с CUPS 2.3 в Debian 11

--- libcups/src/result.rs
+++ libcups/src/result.rs
@@ -1,7 +1,7 @@
 pub fn cups_error() -> Option<String> {
     let error = unsafe { libcups_sys::cupsLastErrorString().as_ref() }?;
 
-    let error = unsafe { core::ffi::CStr::from_ptr(error as *const i8) }
+    let error = unsafe { core::ffi::CStr::from_ptr(error as *const core::ffi::c_char) }
         .to_string_lossy()
         .into_owned();
 
